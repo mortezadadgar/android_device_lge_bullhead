21b64b41b476d1575604f269435acbfd9712093d
elektroschmock <elektroschmock78@googlemail.com>
2019-10-02 15:22:43 +0200

    bullhead: Convert ConfigStore properties to system properties
    Configstore is deprecated since Android 10 due to high memory consumption
    
    Reference:
    https://source.android.com/devices/architecture/configuration
    
    Change-Id: I39ed9422eb95a11153593a7c7b0739e3a994667c

--- device.mk
+++ device.mk
@@ -559,6 +559,13 @@ $(call inherit-product, build/target/product/verity.mk)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.oem_unlock_supported=1
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.max_virtual_display_dimension=2048 \
+    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+
 # Shippig API
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
