8b42ad5c757eba4f229632e34f07e933ec39aa82
mortezadadgar <mortezadadgar97@gmail.com>
2021-02-28 18:21:27 +0000

    bullhead: Remove unneeded/deprecated board flags
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,36 +76,19 @@ WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
 
 BOARD_USES_SECURE_SERVICES := true
 
-#BOARD_HAS_FINGERPRINT_FPC := true
-
 TARGET_NO_RADIOIMAGE := true
 TARGET_BOARD_PLATFORM := msm8992
 TARGET_BOOTLOADER_BOARD_NAME := bullhead
 TARGET_BOARD_INFO_FILE := device/lge/bullhead/board-info.txt
-TARGET_NO_RPC := true
 
 BOARD_EGL_CFG := device/lge/bullhead/egl.cfg
 
-# Shader cache config options
-# Maximum size of the  GLES Shaders that can be cached for reuse.
-# Increase the size if shaders of size greater than 12KB are used.
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-
-# Maximum GLES shader cache size for each app to store the compiled shader
-# binaries. Decrease the size if RAM or Flash Storage size is a limitation
-# of the device.
-MAX_EGL_CACHE_SIZE := 2048*1024
-
-USE_OPENGL_RENDERER := true
 TARGET_USES_ION := true
 TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_GRALLOC1_ADAPTER := true
-TARGET_USES_HWC2 := true
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x2000U | 0x02000000U
 
 TARGET_AUX_OS_VARIANT_LIST := bullhead
-
-HAVE_ADRENO_SOURCE:= false
 
 OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
 
